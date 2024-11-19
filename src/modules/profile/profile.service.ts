e2937b8467a2a91992290c1f03caf5b53df44db6
liuya <1522962872@qq.com>
2024-11-19 15:49:03 +0800

    feat: 修改个人中心 修改密码

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -4,6 +4,9 @@ import { UserInfoByParseToken } from '@/common/dto';
 import { MenuService } from '../system/menu/menu.service';
 import { MenuVo } from '../system/menu/dto/menu.vo';
 import { handleTree } from '@/utils/format';
+import { UpdateProfileDto, UpdatePasswordDto } from './dto/profile.dto';
+import { encryptPassword } from '@/utils/cryptogram';
+import { BusinessException } from '@/common/exceptions';
 
 @Injectable()
 export class ProfileService {
@@ -45,5 +48,31 @@ export class ProfileService {
     } else {
       return handleTree(roleInfo.role.menus);
     }
+  }
+	// 更新个人中心用户信息
+  async updateUserInfo(user: UserInfoByParseToken, info: UpdateProfileDto) {
+    await this.prismaService.user.update({
+      where: {
+        id: user.id
+      },
+      data: info
+    });
+  }
+
+  // 重置密码
+  async updatePassword(user: UserInfoByParseToken, info: UpdatePasswordDto) {
+		const userInfo = await this.prismaService.user.findUnique({
+      where: { id: user.id }
+    });
+		if(userInfo && userInfo.password === encryptPassword(info.oldPassword, userInfo.salt)) {
+			await this.prismaService.user.update({
+				where: { id: user.id },
+				data: {
+					password: encryptPassword(info.newPassword, userInfo.salt)
+				}
+			});
+			return null
+		}
+    BusinessException.throwUsernameOrPasswordIncorrect();
   }
 }
