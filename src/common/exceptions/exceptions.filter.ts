38f19e2ed55034858e6e8cf0f592729916845f3d
hacxy <hacxy.js@outlook.com>
2024-11-20 14:56:11 +0800

    perf: 优化异常过滤器, 修改用户列表返回参数

--- src/common/exceptions/exceptions.filter.ts
+++ src/common/exceptions/exceptions.filter.ts
@@ -2,28 +2,40 @@ import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from
 import type { Request, Response } from 'express';
 import { BusinessException } from './business.exceptions';
 import { BUSINESS_ERROR_CODE } from './business.error.code';
+import { Logger } from 'nestjs-pino';
 
 /**
- *  HTTP 异常过滤器
+ * 异常过滤器
  */
-@Catch(HttpException)
-export class HttpExeptionsFilter implements ExceptionFilter {
+@Catch(HttpException, Error)
+export class ExeptionsFilter implements ExceptionFilter {
+  constructor(private readonly logger: Logger) {}
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const respones = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const error: any = exception.getResponse();
 
     // 处理自定义业务异常
     if (exception instanceof BusinessException) {
+      const error: any = exception.getResponse();
       respones.status(error?.statusCode || respones?.statusCode).send({
         code: error?.code,
         msg: error?.msg[(request.headers.lang as string) || 'zh-CN'] // 返回对应语言的异常信息
       });
       return;
     }
+
+    this.logger.error(
+      `
+path: ${request.url}
+method: ${request.method}
+ip:	${request.ip}
+userInfo: ${JSON.stringify((request as any)?.user)}
+error: ${exception}
+`
+    );
     // 其他异常使用通用状态码返回
-    respones.status(error?.statusCode || respones?.statusCode).send({
+    respones.status(HttpStatus.OK).send({
       code: BUSINESS_ERROR_CODE.COMMON.code,
       msg: BUSINESS_ERROR_CODE.COMMON.msg[(request.headers.lang as string) || 'zh-CN'] // 返回对应语言的异常信息
     });
