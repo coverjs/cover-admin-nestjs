cb3056b97fcaa3c3791e0722811fa34d54f4f7b3
liuya <1522962872@qq.com>
2024-11-20 16:57:54 +0800

    feat: upload file init

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loadSwagger } from './utils/swagger';
 import { Logger } from 'nestjs-pino';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true
   });
+
+  // 配置日志
   app.useLogger(app.get(Logger));
+
+  // 配置跨域
   app.enableCors();
 
-  // load swagger docs
+  // 配置静态资源目录
+  app.useStaticAssets(join(__dirname, `../${process.env.STATIC_DIRECTORY}`), {
+    prefix: '/upload'
+  });
+
+  // 配置 swagger 文档
   loadSwagger(app);
 
   await app.listen(process.env.PORT ?? 1118);
