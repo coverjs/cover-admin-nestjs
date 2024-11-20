21165988e41fa6ac8d85f50b1f6f1372b933341c
liuya <1522962872@qq.com>
2024-11-20 11:04:51 +0800

    feat: 1. excel module 2. 用户列表导出

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { ProfileModule } from './modules/profile/profile.module';
 import { MenuModule } from './modules/system/menu/menu.module';
 import { PermissionAuthGuard } from './common/guard/permission-auth.guard';
 import { IoredisModule } from '@/common/redis/redis.module';
+import { XlsxModule } from '@/common/xlsx/xlsx.module';
+
 import { LoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
@@ -34,7 +36,8 @@ import { LoggerModule } from 'nestjs-pino';
     UserModule,
     RoleModule,
     MenuModule,
-    UploadModule
+    UploadModule,
+    XlsxModule
   ],
   providers: [
     // jwt 校验守卫
--- src/modules/system/user/user.service.ts
+++ src/modules/system/user/user.service.ts
@@ -58,4 +58,33 @@ export class UserService {
     });
     return res;
   }
+
+  async exportJob(queryUserList: UserListDto) {
+    const nameMap: {
+      [key: string]: string;
+    } = {
+      id: '用户ID',
+      username: '用户名',
+      email: '邮箱',
+      nickname: '昵称',
+      role: '角色',
+      enable: '是否可用',
+      createdAt: '创建时间',
+      updatedAt: '更新时间'
+    }; // 如果需要其他字段自己添加
+    const titleName = Object.values(nameMap);
+    const { list } = await this.findList(queryUserList);
+    const xlsxData = list.map((item) => {
+      return Object.keys(nameMap).map((key) => {
+        if (key === 'role') {
+          return item[key].name;
+        }
+        if (key === 'enable') {
+          return item[key] ? '是' : '否';
+        }
+        return item[key];
+      });
+    });
+    return { titleName, xlsxData, fileName: '用户列表' };
+  }
 }
